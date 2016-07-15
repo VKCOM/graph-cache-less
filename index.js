e234e9472367e5ca07e2fdf4f6ee2fc9548b798a
Termina1 <terminal2010@gmail.com>
2016-07-15 21:05:29 +0300

    index in es5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {createGraphFromFile} = require('./build/parser');
-const assign = require('object-assign');
+var parser = require('./build/parser');
+var assign = require('object-assign');
 
 module.exports = function(sign, file, filename, lessOpts) {
-  return createGraphFromFile(file, sign, assign({}, lessOpts, {filename}));
+  return parser.createGraphFromFile(file, sign, assign({}, lessOpts, {filename}));
 }
